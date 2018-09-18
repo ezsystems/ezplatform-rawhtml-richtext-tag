c29fa16362b4f5143ba6363a1eaa93bb02835b54
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-09-18 10:34:22 +0200

    Cleaned up the event subscriber

--- Resources/public/js/richtext_custom_tag_rawhtml.js
+++ Resources/public/js/richtext_custom_tag_rawhtml.js
@@ -1,21 +1,17 @@
-(function (global, eZ) {
+(function () {
     addEventListener('getCustomTagContent', function(e) {
-        console.log("before", e.detail.content);
         e.detail.content = e.detail.content
             .replace(/&amp;/g, '&')
             .replace(/&lt;/g, '<')
             .replace(/&gt;/g, '>')
             .replace(/&quot;/g, '"');
-        console.log("after", e.detail.content);
     });
 
     addEventListener('setCustomTagContent', function(e) {
-        console.log("before", e.detail.content);
         e.detail.content = e.detail.content
             .replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
             .replace(/"/g, '&quot;');
-        console.log("after", e.detail.content);
     });
-})(window, window.eZ);
+})();
